e00dc694e67f907406482f5126b1b8a8301c9ab4
afshinrahmati <afshinrahmati93@gmail.com>
2020-12-18 10:47:29 -0500

    JSONFILE

--- router/index.js
+++ router/index.js
@@ -1,54 +1,31 @@
 const express = require("express");
 const router =  express.Router();
-const axios = require("axios");
+
 const  fs = require('fs')
-axios.get('http://tsetmc.com/tsev2/data/MarketWatchPlus.aspx').then(resp => { 
- let data = resp.data;
- let dataParts = data.split('@');
- let stocks = dataParts[2].split(';');
 
 
- for(let i = 0 ;i<stocks.length;i++){
-   let stockProps = stocks[i].split(',');
      jsonData = `[
-         {
-        "id": "${stockProps[0]}",
-        "link": "${stockProps[1]}",
-        "parent_id": "${stockProps[2]}",
-        "tablecompnt":"${stockProps[3]}",
-        "detail": {
-          "ISIN" : "${stockProps[4]}",
-          "symol" : "${stockProps[5]}",
-          "company" : "${stockProps[6]}",
-          "sharenumber" : "${stockProps[7]}",
-        },
-        "spaciaDelete": "${stockProps[8]}",
-        "price": {
-            "opening" : "${stockProps[9]}",
-            "closeing" : "${stockProps[10]}",
-            "last" : "${stockProps[11]}",
-            "lowest" : "${stockProps[12]}",
-            "heigth" : "${stockProps[13]}",
-            "yesterday" : "${stockProps[14]}",
-          },
-          "cost": "${stockProps[15]}",
-          "value": "${stockProps[16]}",
-          "volume": "${stockProps[17]}",
-        }]
-    ,`
+      {
+        "name":"afshin",
+        "lastname":"rahmati",
+        "phonenumber":"09108348429"
+      }
+        ]`
 
      let make = fs.appendFileSync('sample.json',jsonData, 'utf8');
 
-}
+     let exsic = fs.existsSync('sample.json')
+     if(exsic){
+       let filejson = require("../sample.json");
+       console.log(filejson);
+     }
 
-})
+
+
  
 
 
 
-router.get("/",(req,res,next)=>{
- return res.render("../MarketWatchPlus.aspx")
-})
 
 
 
